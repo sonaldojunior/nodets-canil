4540ea6a38d0eee93c29ef345abf97300e645b11
Sonaldo Junior <sonaldo.junior@hotmail.com>
2022-01-29 09:31:11 -0300

    ajuste no .env

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,18 @@
-import { Request, Response} from 'express';
+import {Request, Response} from 'express';
 
-export const home = (req: Request, res:Response)=>{
+export const home = (req: Request, res: Response)=>{
     res.send('funcionando no controller')
     //res.render('pages/page');
-}
-export const dogs= (req: Request, res:Response)=>{
+};
+export const dogs= (req: Request, res: Response)=>{
+    res.send('funcionando no controller sei lá')
+    //res.render('pages/page');
+};
+export const cats = (req: Request, res: Response)=>{
 
     //res.render('pages/page');
-}
-export const cats = (req: Request, res:Response)=>{
+};
+export const fishes = (req: Request, res: Response)=>{
 
     //res.render('pages/page');
-}
-export const fishes = (req: Request, res:Response)=>{
-
-    //res.render('pages/page');
-}
+};
